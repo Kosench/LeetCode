7fed0d89dc67e5405a4a96851a701fce8ced174d
Kosench <dimako36@mail.ru>
2025-03-31 18:02:32 +1000

    add 238. Product of Array Except Self

---
+++ 238_Product_of_Array_Except_Se/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func productExceptSelf(nums []int) []int {
+	n := len(nums)
+	result := make([]int, n)
+
+	prefix := 1
+	for i := 0; i < n; i++ {
+		result[i] = prefix
+		prefix *= nums[i]
+	}
+
+	suffix := 1
+	for i := n - 1; i >= 0; i-- {
+		result[i] *= suffix
+		suffix *= nums[i]
+	}
+
+	return result
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4}
+	fmt.Println(productExceptSelf(nums))
+}
