5732831d4a89d9437f5a1787107f4c13e0576c7d
Kosench <ilyakosenko09@gmail.com>
2026-06-04 01:02:48 +1000

    add 15

--- Go/Two_Pointer/15_3Sum/main.go
+++ Go/Two_Pointer/15_3Sum/main.go
@@ -2,52 +2,40 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	"slices"
 )
 
 func threeSum(nums []int) [][]int {
-	if len(nums) < 3 {
-		return [][]int{}
-	}
-
-	sort.Ints(nums)
-	result := [][]int{}
-
-	for i := 0; i < len(nums)-2; i++ {
+	slices.Sort(nums)
+	var sl [][]int
+	n := len(nums)
+	for i := 0; i < len(nums); i++ {
 		if i > 0 && nums[i] == nums[i-1] {
 			continue
 		}
-
-		left := i + 1
-		right := len(nums) - 1
-
+		a := nums[i]
+		left, right := i+1, n-1
 		for left < right {
-			sum := nums[i] + nums[left] + nums[right]
-
-			if sum == 0 {
-				result = append(result, []int{nums[i], nums[left], nums[right]})
-
-				for left < right && nums[left] == nums[left+1] {
-					left++
-				}
-				for left < right && nums[right] == nums[right-1] {
-					right--
-				}
-
+			if nums[left]+nums[right] == -a {
+				sl = append(sl, []int{a, nums[left], nums[right]})
 				left++
 				right--
-			} else if sum < 0 {
+				for left < right && nums[left] == nums[left-1] {
+					left++
+				}
+				for left < right && nums[right] == nums[right+1] {
+					right--
+				}
+			} else if nums[left]+nums[right] < -a {
 				left++
 			} else {
 				right--
 			}
 		}
 	}
-
-	return result
+	return sl
 }
 
 func main() {
-	nums := []int{-1, 0, 1, 2, -1, -4}
-	fmt.Println(threeSum(nums))
+	fmt.Println(threeSum([]int{-1, 0, 1, 2, -1, -4}))
 }
